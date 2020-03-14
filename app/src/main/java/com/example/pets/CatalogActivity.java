ebb6a11c1c8b6628093ec7ac3cd0edceede0050b
shankha shubhra sanyal <shankh@gmail.com>
2020-03-14 18:14:20 +0530

    1.1 LAYOUTS DONE!

--- app/src/main/java/com/example/pets/CatalogActivity.java
+++ app/src/main/java/com/example/pets/CatalogActivity.java
@@ -2,7 +2,11 @@ package com.example.pets;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class CatalogActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class CatalogActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CatalogActivity.this, EditorActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/pets/EditorActivity.java
@@ -0,0 +1,15 @@
+package com.example.pets;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EditorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+    }
+
+}

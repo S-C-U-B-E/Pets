2a394dcafbb885c1f6b9cbe6c340fc9c5507a5f4
S-C-U-B-E <sssanyal10@gmail.com>
2020-03-18 00:37:02 +0530

    1.3 Working on Content Provider..

--- app/src/main/java/com/example/pets/data/PetsContract.java
+++ app/src/main/java/com/example/pets/data/PetsContract.java
@@ -1,12 +1,19 @@
 package com.example.pets.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public final class PetsContract {
 
+    public static final String CONTENT_AUTHORITY = "com.example.android.pets";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://"+ CONTENT_AUTHORITY);
+    public static final String PATH_PETS = "pets";
+
     private PetsContract() {}  //To prevent un-intentional instantiation of this calss
 
     public static final class PetsEntry implements BaseColumns{
+
+        public static final Uri CONTENT_URI   = Uri.withAppendedPath(BASE_CONTENT_URI,PATH_PETS);
 
         public static final String TABLE_NAME = "pets";
 
